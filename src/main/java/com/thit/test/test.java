22e4f3d9c664cfbd07b0cba53816c04a20fa3dc3
huyiju <56994699+huyiju@users.noreply.github.com>
2020-05-09 15:45:17 +0800

    Update test.java

--- src/main/java/com/thit/test/test.java
+++ src/main/java/com/thit/test/test.java
@@ -6,6 +6,6 @@ package com.thit.test;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("master1111");
+        System.out.println("主分支");
     }
 }
