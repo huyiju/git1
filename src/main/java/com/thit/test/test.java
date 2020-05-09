1a6f95d51c02a2d2f9fc7a294fe11e94179a1664
huyiju <790270807@qq.com>
2020-05-09 15:41:37 +0800

    5.5.2 删除test3

--- src/main/java/com/thit/test/test.java
+++ src/main/java/com/thit/test/test.java
@@ -4,7 +4,7 @@ package com.thit.test;
  * @author :huyiju
  * @date :2020-05-09 14:31
  */
-public class test1 {
+public class test {
     public static void main(String[] args) {
         System.out.println("master1111");
     }
