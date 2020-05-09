35cf0e7e82831816482f5b9ffabfabccc2a3ff0d
huyiju <790270807@qq.com>
2020-05-09 15:15:20 +0800

    5.5.2 新建test3

---
+++ src/main/java/com/thit/test/test3.java
@@ -0,0 +1,11 @@
+package com.thit.test;
+
+/**
+ * @author :huyiju
+ * @date :2020-05-09 14:31
+ */
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
