b54dc4fb4fdb5407975a53769e74f9df42c87931
huyiju <790270807@qq.com>
2020-05-09 15:47:36 +0800

    5.5.2 测试环境

--- src/main/java/com/thit/test/test.java
+++ src/main/java/com/thit/test/test.java
@@ -6,6 +6,6 @@ package com.thit.test;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("mastet1");
+        System.out.println("test环境");
     }
 }
